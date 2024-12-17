5fea849f28a8e3b898bc449ad062ced2d283a3ce
Connor Cluett <cpcluett@syr.edu>
2024-12-16 23:20:39 -0500

    updated stuff

--- code/api_get_sports_key.py
+++ code/api_get_sports_key.py
@@ -31,3 +31,21 @@ if __name__ == "__main__":
     # Save the DataFrame to the cache folder
     file_path = os.path.join(cache_dir, "sports.csv")
     sports.to_csv(file_path, index=False)
+
+    output_dir = "cache"
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Read the CSV file
+    sports_df = pd.read_csv('./cache/sports.csv')
+
+    # Create two dataframes based on the 'has_outrights' column
+    sports_futures_df = sports_df[sports_df['has_outrights'] == True]
+    sports_matches_df = sports_df[sports_df['has_outrights'] == False]
+
+    # Define file paths
+    futures_file = os.path.join(output_dir, 'sports_futures.csv')
+    matches_file = os.path.join(output_dir, 'sports_matches.csv')
+
+    # Write the dataframes to separate CSV files
+    sports_futures_df.to_csv(futures_file, index=False)
+    sports_matches_df.to_csv(matches_file, index=False)
